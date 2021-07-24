f5d6b7ecf32b2394b91b0c05e7ca7fd1acbf4ac7
Huy Ngu <huy.ngu@compass.com>
2021-07-24 06:53:56 -0400

    add prettier

--- src/frontend/modules/starter-pack/src/index.tsx
+++ src/frontend/modules/starter-pack/src/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
 
-const App = () => (
-  <h1>My React and TypeScript App!</h1>
-);
+const App = () => <h1>My React and TypeScript App!</h1>;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
