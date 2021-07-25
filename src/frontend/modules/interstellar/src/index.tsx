145bc0af5c26af92ee7795aa953341f379234d99
Huy Ngu <huy.ngu@compass.com>
2021-07-25 18:46:24 -0400

    test styled components

--- src/frontend/modules/interstellar/src/index.tsx
+++ src/frontend/modules/interstellar/src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { GlobalStyles } from './globals';
 
 const App = () => <h1>My React and TypeScript App!</h1>;
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
