8ae6e8aa03849f82bfc3baef73805627cff592e9
Huy Ngu <huy.ngu@compass.com>
2021-07-27 22:13:23 -0400

    adding babel plugin for styling with macros

--- src/frontend/modules/interstellar/src/components/Button/Button.stories.tsx
+++ src/frontend/modules/interstellar/src/components/Button/Button.stories.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Story } from '@storybook/react';
 import { Button, Props } from './Button';
+import { Center } from '../utils/Center/Center';
+
+import { addDecorator } from '@storybook/react';
+
+type DecoratorFunction = Parameters<typeof addDecorator>[0];
+
+export interface StoryMetadata {
+  component: React.ReactNode;
+  title: string;
+  decorators?: DecoratorFunction[];
+}
 
 export default {
   title: 'Inputs/Button', // should be unique name
   component: Button,
-};
+  decorators: [(storyFn) => <Center>{storyFn()}</Center>],
+} as StoryMetadata;
 
 const Template: Story<Props> = (args) => <Button {...args} />;
 
