899053a666cd7894e2bbb6e22e6bb1d5e53ed696
Huy Ngu <huy.ngu@compass.com>
2021-09-02 20:48:07 -0400

    adding todo reducers and selectors

--- src/frontend/apps/sandbox/src/pages/ToDoPage/TodoList.tsx
+++ src/frontend/apps/sandbox/src/pages/ToDoPage/TodoList.tsx
@@ -1,10 +1,12 @@
 import React, { FC, useEffect, useState } from 'react';
 import { useAppSelector, useAppDispatch } from './hooks';
-import { setTodos } from './todoListSlice';
+import styled from 'styled-components/macro';
+import { setTodos, removeTodo, updateTodo, Todo, getTotalTodos } from './todoListSlice';
 
 const TodoList: FC = () => {
   const dispatch = useAppDispatch();
   const todoList = useAppSelector((state) => state.todoList.todos);
+  const totalTodos = useAppSelector(getTotalTodos);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -23,15 +25,54 @@ const TodoList: FC = () => {
     };
   }, [dispatch]);
 
-  return todoList.length > 0 ? (
-    <ul>
-      {todoList.map((todo) => (
-        <li key={todo.id}>{todo.name}</li>
-      ))}
-    </ul>
+  const handleRemoveTodoClick = (id: string) => {
+    dispatch(removeTodo(id));
+  };
+
+  const handleToggleTodoActive = (todo: Todo) => {
+    dispatch(
+      updateTodo({
+        ...todo,
+        isActive: !todo.isActive,
+      }),
+    );
+  };
+
+  return totalTodos > 0 ? (
+    <>
+      <p>You have {totalTodos} todos </p>
+      <ul>
+        {todoList.map((todo) => (
+          <StyledListItem
+            key={todo.id}
+            onClick={() => {
+              handleToggleTodoActive(todo);
+            }}
+            isActive={todo.isActive}
+          >
+            {todo.name}
+            <button
+              onClick={() => {
+                handleRemoveTodoClick(todo.id);
+              }}
+            >
+              x
+            </button>
+          </StyledListItem>
+        ))}
+      </ul>
+    </>
   ) : !loading ? (
     <p>Please enter a todo</p>
   ) : null;
 };
 
 export default TodoList;
+
+type StyledListItemProps = {
+  isActive: boolean;
+};
+
+const StyledListItem = styled.li<StyledListItemProps>`
+  text-decoration: ${(props) => (props.isActive ? 'none' : 'line-through')};
+`;
--- src/frontend/apps/sandbox/src/pages/ToDoPage/todoListSlice.ts
+++ src/frontend/apps/sandbox/src/pages/ToDoPage/todoListSlice.ts
@@ -1,7 +1,8 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction, createSelector } from '@reduxjs/toolkit';
+import { RootState } from './store';
 
 // 1. setup the type definition of the state
-type Todo = {
+export type Todo = {
   id: string;
   name: string;
   isActive: boolean;
@@ -24,11 +25,44 @@ const todoListSlice = createSlice({
     setTodos(state, action: PayloadAction<Todo[]>) {
       state.todos = action.payload;
     },
+    addTodo(state, action: PayloadAction<Todo>) {
+      state.todos = [...state.todos, action.payload];
+    },
+    removeTodo(state, action: PayloadAction<string>) {
+      const id = action.payload;
+      state.todos = state.todos.filter((todo) => todo.id != id);
+    },
+    updateTodo(state, action: PayloadAction<Todo>) {
+      const updatedTodo = action.payload;
+      const currentTodoIndex = state.todos.findIndex((todo) => todo.id === updatedTodo.id);
+      if (currentTodoIndex === -1) {
+        return;
+      }
+      state.todos[currentTodoIndex] = updatedTodo;
+    },
   },
 });
 
 // export action creators
-export const { setTodos } = todoListSlice.actions;
+export const { setTodos, addTodo, removeTodo, updateTodo } = todoListSlice.actions;
 
 // 4. export the slice reducer
 export default todoListSlice.reducer;
+
+// create reusable, memoized selectors here
+/**
+ * NOTE: createSelector allows you to aggregate multiple slices
+ * as long as the last param is a function that does something with those slices.
+ *
+ * EX:
+ *
+ * export const foo = createSelector(
+  (state: RootState) => state.a.b,
+  (state: RootState) => state.c.d,
+  (b,d) => return b + d,
+);
+ */
+export const getTotalTodos = createSelector(
+  (state: RootState) => state.todoList.todos,
+  (todos) => todos.length,
+);
