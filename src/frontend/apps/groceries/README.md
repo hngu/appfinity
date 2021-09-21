9bc065af04ca9443f89a07de31fcb6a2ab6bba35
Huy Ngu <huy.ngu@compass.com>
2021-09-21 07:41:15 -0400

    adding slice file

--- src/frontend/apps/groceries/README.md
+++ src/frontend/apps/groceries/README.md
@@ -15,3 +15,8 @@ Go inside this directory and run `rm -rf node_modules/ && tar cvf - . | (cd /des
 1. Install `react-redux`, `@reduxjs/toolkit` and `@types/react-redux`
 1. Create a store file and configure it with an empty reducer
 1. Then wrap your app in a Provider from redux and pass in the store
+1. Here is what we have so far: https://github.com/hngu/appfinity/commit/10cd4478597e33460a96f11ac1907562b506dfc1
+1. Create a slice file.
+1. In the slice file, setup the initial state.
+1. In the slice file, setup the slice.
+1. Finally, export the slice's action creators (via slice.actions) and the slice's reducer via (slice.reducer)
---
+++ src/frontend/apps/groceries/src/groceriesSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { GroceryItem } from './types';
+
+// 1. Setup type definitions for the redux state
+type GroceriesState = {
+  groceries: GroceryItem[];
+};
+
+// 2. Create the initial state
+const initialState: GroceriesState = {
+  groceries: [],
+};
+
+// 3. Create the slice
+const groceriesSlice = createSlice({
+  name: 'groceries',
+  initialState,
+  reducers: {
+    setGroceries(state, action: PayloadAction<GroceryItem[]>) {
+      state.groceries = action.payload;
+    },
+  },
+});
+
+// 4. export the action creators
+export const { setGroceries } = groceriesSlice.actions;
+
+// 5. export the slice reducer
+export default groceriesSlice.reducer;
